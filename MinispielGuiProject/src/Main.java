486373c6f1a3e080a4084ab2b2ed7a798d9b83c4
m-sul <m-sul@DESKTOP-AHK8QII.fritz.box>
2024-09-01 18:14:25 +0200

    Initial commit.

---
+++ MinispielGuiProject/src/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	Label label;
+	Label secondLabel;
+	Label thirdLabel;
+	Button button1;
+	Button button2;
+	Button button3;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
