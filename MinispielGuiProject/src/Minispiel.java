7a823b90e3a978e4a715f8bde8b8e4a74261f281
m-sul <m-sul@DESKTOP-AHK8QII.fritz.box>
2024-09-01 18:29:07 +0200

    Test1

--- MinispielGuiProject/src/Minispiel.java
+++ MinispielGuiProject/src/Minispiel.java
@@ -1,21 +1,72 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Minispiel extends Application {
 	
-	Label label;
-	Label secondLabel;
-	Label thirdLabel;
-	Button button1;
-	Button button2;
-	Button button3;
+	private Label label;
+	private Label secondLabel;
+	private Label thirdLabel;
+	
+	private Button button1;
+	private Button button2;
+	private Button button3;
+	
+	private GridPane gp;
+	
+	private VBox root;
+	
+	private Scene scene;
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		// TODO Auto-generated method stub
 		
+		label = new Label("Minispiel");
+		
+		button1 = new Button("Click Me.");
+		button2 = new Button("Klicke auf mich!");
+		button3 = new Button("Klicke NICHT auf mich!");
+		
+		button1.setOnAction(event -> {
+			button1.setText("Du hast auf den Knopf gedrückt!");
+		});
+		
+		button2.setOnAction(event -> {
+			button2.setText("Du hast auf den zweiten Knopf gedrückt!");
+		});
+		
+		button3.setOnAction(event -> {
+			button3.setText("Du solltest nicht auf den Knopf drücken!");
+		});
+		
+		root = new VBox(10);
+		root.getChildren().add(button1);
+		
+		scene = new Scene(root, 450, 300);
+		stage.setScene(scene);
+		
+		stage.setTitle("Mein Minispiel");
+		
+		stage.show();
+		
+	}
+	
+	private void showAlert(String title, String content) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		alert.showAndWait();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 	
 	
